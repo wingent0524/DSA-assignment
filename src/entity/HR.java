e0a17af2208d338e26f51b7e16a5f5c8bfcbb57c
DANSO <DANSO@DESKTOP-OF28PKV>
2017-12-24 21:33:42 +0800

    ModuleD(Tneh)

---
+++ src/entity/HR.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author MSI-
+ */
+public class HR implements Comparable<HR>{
+    private String staffID;
+    private String password;
+
+    public HR(String staffID, String password) {
+        this.staffID = staffID;
+        this.password = password;
+    }
+    
+    public HR(){
+        
+    }
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int compareTo(HR o) {
+      return this.getStaffID().compareTo(o.getStaffID());
+    }
+    
+    
+}
